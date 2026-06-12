b998069716a8839e0005b93a733950b2ec12e318
Samiran Das <dassamiran4368@gmail.com>
2026-06-12 15:51:49 +0530

    Learn About Comparators, Math Methods, conditional and itertion statements

---
+++ J.S/2JS/Comparative.js
@@ -0,0 +1,24 @@
+// console.log(2*2)
+console.log(2 + "2")
+console.log(2 + 4 + "2")
+console.log("2"  + 2 + 4 + "2")
+
+
+let  x = 2
+let y = "2"
+console.log(x === y)
+console.log(x == y)
+console.log(null == undefined)
+console.log(null === undefined)
+
+console.log(null == 0)
+console.log(null === 0)
+console.log(null >=0)
+console.log(null <=0)
+console.log(null >=-1)
+console.log(null >=1)
+
+console.log("2" >= 2)
+
+
+
---
+++ J.S/2JS/Variable.js
@@ -0,0 +1,16 @@
+let c = "7";
+console.log(typeof(c))
+
+let t = Number(c);
+console.log(typeof(t))
+
+
+var a = null;
+console.log(a);
+console.log(typeof(a))
+
+
+let f = "Pavni"
+let g = Number(f)
+console.log(g)
+console.log(typeof(g))
