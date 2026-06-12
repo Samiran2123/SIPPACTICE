f260e79a5d9a696264e118f8bf57214f1958930c
Samiran Das <dassamiran4368@gmail.com>
2026-06-12 12:31:56 +0530

    Css end and js data types

---
+++ J.S/1JS/Variable.js
@@ -0,0 +1,9 @@
+let c = "7";
+console.log(typeof(c))
+
+let t = Number(c);
+console.log(typeof(t))
+
+
+var a = null;
+console.log(a);
---
+++ J.S/1JS/script.js
@@ -0,0 +1,43 @@
+console.log("Hann bhai agaye swaad ")
+alert("Kyun nahi ho rahi padhai")
+let mehul = "yashwant";
+console.log(mehul);
+
+mehul = "jain";
+console.log(mehul);
+
+mehul = 20;
+console.log(mehul+"Jain");
+let kaya = "Kaya hal hai"
+
+const Yashwant = "Mehul"
+console.log(Yashwant );
+
+// Yashwant = "Prasad";
+// console.log(Yashwant);
+
+console.log(`Kaya bhai ${kaya}` );
+
+
+let a = 20;
+// console.log(a)
+var b = 30;
+console.log(b)
+
+const c = 50;
+console.log(c)
+{
+   
+//    let a = 10;
+    console.log(a)
+    // var b =40;
+    // console.log(b)
+
+    const c = 60;
+    console.log(c)
+
+}
+
+console.log(a)
+console.log(b)
+console.log(c)
